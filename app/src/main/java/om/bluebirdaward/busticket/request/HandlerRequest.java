64c6cc18f425d7df9751fd44e386ad219256c9a2
wintb <mailtestapp01@gmail.com>
2016-07-20 23:51:11 +0700

    [Vũ Văn Thắng] update code

--- app/src/main/java/om/bluebirdaward/busticket/request/HandlerRequest.java
+++ app/src/main/java/om/bluebirdaward/busticket/request/HandlerRequest.java
@@ -3,16 +3,18 @@ package om.bluebirdaward.busticket.request;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import om.bluebirdaward.busticket.mics.Constant;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class HandlerRequest {
 
+
     private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder().readTimeout(1, TimeUnit.MINUTES)
             .connectTimeout(1, TimeUnit.SECONDS).writeTimeout(1, TimeUnit.SECONDS);
     private static Retrofit.Builder builder = new Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(Constant.host)
             .addConverterFactory(GsonConverterFactory.create());
 
     public static <S> S createService(Class<S> serviceClass) {
